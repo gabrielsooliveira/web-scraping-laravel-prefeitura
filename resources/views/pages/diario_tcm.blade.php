36f12679d6691e65176a827efa98e62c62b77471
Gabriel <gabrielsouzacontato.99@gmail.com>
2024-08-02 17:16:26 -0300

    feat: diario do tcm atualização

--- resources/views/pages/diario_tcm.blade.php
+++ resources/views/pages/diario_tcm.blade.php
@@ -2,42 +2,27 @@
 
 @section('content')
 <div class="container">
-   <div class="mt-4">
-        <nav aria-label="Page navigation example" class="mb-2 d-flex">
-            <span class="mt-2">{{ $resultados->currentPage() }} de {{ $resultados->lastPage() }} Páginas</span>
-            <ul class="pagination ms-auto p-0">
-                <li class="page-item">
-                    <a class="page-link link-dark {{ $resultados->currentPage() === 1 ? 'disabled' : '' }}" href="{{ $resultados->previousPageUrl() }}"> << Anterior </a>
-                </li>
-                <li class="page-item">
-                    <a class="page-link link-dark {{ $resultados->currentPage() === $resultados->lastPage() ? 'disabled' : '' }}" href="{{ $resultados->nextPageUrl() }}"> Proxima >> </a>
-                </li>
-            </ul>
-        </nav>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">DOM</th>
-                    <th scope="col">Data de publicação</th>
-                    <th scope="col">Informações</th>
-                    <th scope="col">PDF</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($resultados as $value)
-                <tr>
-                    <th scope="row">{{ $value['codigo'] }}</th>
-                    <td>{{ date('d/m/Y', strtotime($value['data_publicacao'])) }}</td>
-                    <td>
-                    @if ($value->exoneracoes->isNotEmpty())
-                    <span class="badge text-bg-warning">Exonerações Existentes</span>
-                    @endif
-                    </td>
-                    <td><a type="button" class="btn btn-primary btn-sm" href="{{ Storage::url('DOM/' . $value['url']) }}" target="_blank">Visualizar</a></td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-   </div>
+    <div class="mt-5 card p-4" style="height:600px">
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">DOM</th>
+                        <th scope="col">Data de publicação</th>
+                        <th scope="col">PDF</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($resultados["itens"] as $value)
+                    <tr>
+                        <th scope="row">DOM - {{ $value['numero'] }}</th>
+                        <td>{{ date('d/m/Y', strtotime($value['data'])) }}</td>
+                        <td><a type="button" class="btn btn-primary btn-sm" href="https://egbanet.egba.ba.gov.br/tcm/ver-pdf/{{ $value['id'] }}/#/p:1/e:{{ $value['id'] }}" target="_blank">Visualizar</a></td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
 @endsection
